762acc1b18d996d7738c60595825799e20391cbe
FMG <funtasticaarav@gmail.com>
2025-10-30 01:04:53 +0530

    double pendulum is complete lets goooo

--- main.py
+++ main.py
@@ -7,43 +7,57 @@ import matplotlib.animation as animation
 dt=0.03
 t=20
 frames = int(t//dt)
+
+
 m1 = 10
 m2 = 10
-l = 1
-L = 2 * l
+M1 = 10
+M2 = 10
+l1 = 1
+l2 = 1
+g = 9.81
+o1=radians(90)
+o2=radians(135)
+
+
+L = l1+l2
 x1=[]
 y1=[]
 x2=[]
 y2=[]
-g = 9.81
-n=0.1
-R=1
-
-o1=radians(90)
-o2=radians(135)
 
 def calc():
     w1=0
     w2=0
-    global x1,y1,m1,o1,g,frames,dt,l,n,R
-    global x2,y2,m2,o2
+    global x1,y1,m1,M1,l1,o1,g,frames,dt,n,R
+    global x2,y2,m2,M2,l2,o2
 
     j=-1
     while(j<frames):
         # a=-g * sin(o) / l - 6*pi*n*R*w/m
-        a1=-g * sin(o1) / l
+        K1 = -(m2*(w2**2)*l1*l2*sin(o1-o2)) - (g*l1*sin(o1)*((M1+M2)/2 + m1+m1))
+        K2 = (m2*(w1**2)*l1*l2*sin(o1-o2)) - (g*l2*sin(o2)*((M2)/2 + m2))
+        A1 = m1*(l1**2) + m2*(l2**2) + M1*(l1**2)/3
+        B1 = m2*l1*l2*cos(o1-o2)
+        A2 = m2*l1*l2*cos(o1-o2)
+        B2 = m2*(l2**2) + M2*(l2**2)/3
+        D = A1*B2 - A2*B1
+
+        a1 = (B2*K1-B1*K2)/D
+        a2 = (A1*K2-A2*K1)/D
+
+
         w1+=a1*dt
         o1+=w1*dt
 
-        a2=-g * sin(o2) / l
         w2+=a2*dt
         o2+=w2*dt
 
-        curx1=l*sin(o1)
-        cury1=-l*cos(o1)
+        curx1=l1*sin(o1)
+        cury1=-l1*cos(o1)
 
-        curx2=curx1 + l*sin(o2)
-        cury2=cury1 - l*cos(o2)
+        curx2=curx1 + l2*sin(o2)
+        cury2=cury1 - l2*cos(o2)
 
 
         x1.append(curx1)
@@ -83,5 +97,5 @@ ani = animation.FuncAnimation(
     fig, animate, frames, interval=dt*1000, blit=True)
 
 writer = animation.PillowWriter(fps=30,bitrate=1800)
-ani.save('doublesimple.gif', writer=writer)
+ani.save('double.gif', writer=writer)
 # plt.show()
